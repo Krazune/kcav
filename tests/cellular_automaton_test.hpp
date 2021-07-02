b79824dc8fc60e7326731dc55425cfda6d4f0047
Krazune <krazune@gmail.com>
2021-07-02 14:55:33 +0100

    Add cellular automaton tests

---
+++ tests/cellular_automaton_test.hpp
@@ -0,0 +1,49 @@
+#ifndef CELLULAR_AUTOMATON_TEST_HPP
+#define CELLULAR_AUTOMATON_TEST_HPP
+
+#include <memory>
+
+#include <gtest/gtest.h>
+
+#include "../source/cellular_automaton.hpp"
+
+#include "mocks/neighbors_selector_mock.hpp"
+#include "mocks/ruleset_mock.hpp"
+
+class CellularAutomatonTest : public ::testing::Test
+{
+	protected:
+	std::unique_ptr<ruleset_mock> rulesetMock = std::make_unique<ruleset_mock>();
+	std::unique_ptr<neighbors_selector_mock> neighborsSelectorMock = std::make_unique<neighbors_selector_mock>();
+
+	sf::Image colorfulImage; // Red Green Blue, Cyan Magenta Yellow.
+
+	CellularAutomatonTest()
+	{
+		colorfulImage.create(3, 2, sf::Color::Red);
+		colorfulImage.setPixel(1, 0, sf::Color::Green);
+		colorfulImage.setPixel(2, 0, sf::Color::Blue);
+		colorfulImage.setPixel(0, 1, sf::Color::Cyan);
+		colorfulImage.setPixel(1, 1, sf::Color::Magenta);
+		colorfulImage.setPixel(2, 1, sf::Color::Yellow);
+	}
+};
+
+TEST_F(CellularAutomatonTest, GetNextGenerationTest)
+{
+	EXPECT_CALL(*rulesetMock, get_state(::testing::_, ::testing::_)).Times(6).WillRepeatedly(::testing::Return(sf::Color::Red));
+	EXPECT_CALL(*neighborsSelectorMock, get_neighbors(::testing::_, ::testing::_, ::testing::_)).Times(6).WillRepeatedly(::testing::Return(std::vector<sf::Color>(8, sf::Color::Black)));
+
+	kcav::cellular_automaton cellularAutomaton(std::move(rulesetMock), std::move(neighborsSelectorMock));
+
+	const sf::Image nextGeneration = cellularAutomaton.get_next_generation(colorfulImage);
+
+	ASSERT_EQ(nextGeneration.getPixel(0, 0), sf::Color::Red);
+	ASSERT_EQ(nextGeneration.getPixel(1, 0), sf::Color::Red);
+	ASSERT_EQ(nextGeneration.getPixel(2, 0), sf::Color::Red);
+	ASSERT_EQ(nextGeneration.getPixel(0, 1), sf::Color::Red);
+	ASSERT_EQ(nextGeneration.getPixel(1, 1), sf::Color::Red);
+	ASSERT_EQ(nextGeneration.getPixel(2, 1), sf::Color::Red);
+}
+
+#endif
