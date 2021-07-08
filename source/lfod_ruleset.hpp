a82948624b6313b815334f52b2dac941673b8ea8
Krazune <krazune@gmail.com>
2021-07-08 14:15:56 +0100

    Add lfod automaton

---
+++ source/lfod_ruleset.hpp
@@ -0,0 +1,18 @@
+#ifndef KCAV_LFOD_RULESET_HPP
+#define KCAV_LFOD_RULESET_HPP
+
+#include "ruleset.hpp"
+
+namespace kcav
+{
+	class lfod_ruleset : public ruleset
+	{
+		public:
+		sf::Color get_state(sf::Color selfState, std::vector<sf::Color> neighbors) override;
+
+		bool has_invalid_states(const sf::Image& image) override;
+		sf::Image convert_invalid_states(const sf::Image& original) override;
+	};
+}
+
+#endif
